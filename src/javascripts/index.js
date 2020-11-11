5b47bf637c76a33cbc37c2ec0fcc8a973aff508a
Mr-generic3 <42620430+Mr-generic3@users.noreply.github.com>
2020-11-10 21:43:41 -0700

    final submission

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -4,6 +4,47 @@ require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
 require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
-//TODO
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+//TODO
+import { movies } from './movies'
+
+let featured_movie = document.querySelector('.featured')
+for(let m of movies){
+    let movies_thumb = document.getElementById('m' + m.id)
+    movies_thumb.innerHTML = `
+    <img src="${m.url}">
+    `
+
+    movies_thumb.onclick = function(){
+        selectMovie(m)
+    }
+}
+
+function selectMovie(m){
+    featured_movie.innerHTML = `
+    <img src="${m.url}" style="width: 100%;">
+    <h2>${m.title}</h2>
+    <p>${m.description}</p>
+    `
+}
+
+function searchMovies(event){
+    if(event){
+        event.preventDefault()
+    }
+
+    for(let m of movies){
+        let movies_thumb = document.getElementById('m' + m.id)
+        if(m.title.toUpperCase().indexOf(input.toUpperCase()) == -1){
+           // movies_thumb.classList.add('hidden')
+           movies_thumb.style.display = 'none'
+        } else{
+           // movies_thumb.classList.remove('hidden')
+           movies_thumb.style.display = 'block'
+        }
+    }
+}
+document.querySelector('button').onclick = searchMovies
+document.forms[0].addEventListener('submit', searchMovies, false)
---
+++ src/javascripts/movies.js
@@ -0,0 +1,73 @@
+export const movies = [{
+    "id": 1,
+    "title": "Khorsan Iran",
+    "url":"https://images.unsplash.com/photo-1536257104079-aa99c6460a5a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    "description":"Lorem ipsum dolor sit amet, consectetur adispiscing elic, sed do eiusmod tempor incididunt it labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ulamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit."
+  },
+  {
+    "id": 2,
+    "title": "Atacama Chile",
+    "url":"https://images.unsplash.com/photo-1508556497405-ed7dcd94acfc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    "description":"Lorem ipsum dolor sit amet, consectetur adispiscing elic, sed do eiusmod tempor incididunt it labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ulamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit."
+  },
+  {
+    "id": 3,
+    "title": "Red Cliff United States",
+    "url":"https://images.unsplash.com/photo-1470240731273-7821a6eeb6bd?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    "description":"Lorem ipsum dolor sit amet, consectetur adispiscing elic, sed do eiusmod tempor incididunt it labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ulamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit."
+  },
+  {
+    "id": 4,
+    "title": "Moab United States",
+    "url":"https://images.unsplash.com/photo-1516298773066-c48f8e9bd92b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    "description":"Lorem ipsum dolor sit amet, consectetur adispiscing elic, sed do eiusmod tempor incididunt it labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ulamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit."
+  },
+  {
+    "id": 5,
+    "title": "Iceland",
+    "url":"https://images.unsplash.com/photo-1543503484-ba590cb1f903?ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80",
+    "description":"Lorem ipsum dolor sit amet, consectetur adispiscing elic, sed do eiusmod tempor incididunt it labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ulamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit."
+  },
+  {
+    "id": 6,
+    "title": "Pelasgia Greece",
+    "url":"https://images.unsplash.com/photo-1519732366889-d6cef8036ebb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1326&q=80",
+    "description":"Lorem ipsum dolor sit amet, consectetur adispiscing elic, sed do eiusmod tempor incididunt it labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ulamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit."
+  },
+  {
+    "id": 7,
+    "title": "Grimentz switzerland",
+    "url":"https://images.unsplash.com/photo-1515961896317-adf9e14bdcc0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    "description":"Lorem ipsum dolor sit amet, consectetur adispiscing elic, sed do eiusmod tempor incididunt it labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ulamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit."
+  },
+  {
+    "id": 8,
+    "title": "Kauai United States",
+    "url":"https://images.unsplash.com/photo-1505852679233-d9fd70aff56d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    "description":"Lorem ipsum dolor sit amet, consectetur adispiscing elic, sed do eiusmod tempor incididunt it labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ulamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit."
+  },
+  {
+    "id": 9,
+    "title": "Switzerland",
+    "url":"https://images.unsplash.com/photo-1563791877359-4a03fb576945?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1225&q=80",
+    "description":"Lorem ipsum dolor sit amet, consectetur adispiscing elic, sed do eiusmod tempor incididunt it labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ulamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit."
+  },
+  {
+    "id": 10,
+    "title": "Eagle Canyon Canada",
+    "url":"https://images.unsplash.com/photo-1533692328991-08159ff19fca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    "description":"Lorem ipsum dolor sit amet, consectetur adispiscing elic, sed do eiusmod tempor incididunt it labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ulamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit."
+  },
+  {
+    "id": 11,
+    "title": "Penrith UK",
+    "url":"https://images.unsplash.com/photo-1600337752115-de2c09d6704f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    "description":"Lorem ipsum dolor sit amet, consectetur adispiscing elic, sed do eiusmod tempor incididunt it labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ulamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit."
+  },
+  {
+    "id": 12,
+    "title": "Lake Atitlan Guatemala",
+    "url":"https://images.unsplash.com/photo-1532274402911-5a369e4c4bb5?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    "description":"Lorem ipsum dolor sit amet, consectetur adispiscing elic, sed do eiusmod tempor incididunt it labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ulamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit."
+  }
+]
